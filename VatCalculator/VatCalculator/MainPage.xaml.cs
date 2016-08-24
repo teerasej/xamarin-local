c0a6a1d1845841bf82aa2cceb15e83e4afa2c9d2
teerasej <teerasej@gmail.com>
2016-08-24 10:20:24 +0700

    First Commit

---
+++ VatCalculator/VatCalculator/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VatCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Calculate(object sender, EventArgs e)
+        {
+            string money = entryMoney.Text;
+            double result = double.Parse(money) * 107 / 100;
+            labelResult.Text = result.ToString();
+        }
+    }
+}
